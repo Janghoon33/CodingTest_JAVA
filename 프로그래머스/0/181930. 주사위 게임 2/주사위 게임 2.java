5a32c0216f4620ebc2ec1c432635dbba86c727a7
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2024-03-22 10:39:26 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        int oneNum = a+b+c;
+        int twoNum = (int) (Math.pow(a,2) + Math.pow(b,2) + Math.pow(c,2));
+        int threeNum = (int) (Math.pow(a,3) + Math.pow(b,3) + Math.pow(c,3));
+        
+        answer = (a==b && b==c) ? oneNum*twoNum*threeNum : ((a==b || a==c || b==c) ? oneNum*twoNum : oneNum) ;
+        
+        return answer;
+    }
+}
