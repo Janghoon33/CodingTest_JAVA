a094b6cd09267ef66cb2d805b6c7462b9954b59e
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2024-03-27 15:27:54 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.74 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -3,10 +3,7 @@ class Solution {
         char[] cntArr = control.toCharArray();
         
         for(int i = 0; i < cntArr.length; i++){
-            if (cntArr[i] == 'w') n += 1;
-            else if (cntArr[i] == 's') n -= 1;
-            else if (cntArr[i] == 'd') n += 10;
-            else if (cntArr[i] == 'a') n -= 10;
+            n = cntArr[i] == 'w' ? n+1 : (cntArr[i] == 's' ? n-1 : (cntArr[i] == 'd') ? n+10 : n-10);
         }
         
         return n;
