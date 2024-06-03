7e1a774eaf8b22173633f7f9474becbef485aeaf
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2024-06-03 23:09:26 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.02 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(String number) {
-        int answer = 0;
+       int sum = 0;
 
-        for(int i=0; i<number.length(); i++) {
-            answer += number.charAt(i) - '0';
+       for (char c : number.toCharArray()) {
+            sum += c - '0';
         }
 
-        answer %= 9;
-
-        return answer;
+        return sum % 9;
     }
 }
